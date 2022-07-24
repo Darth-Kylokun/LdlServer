2e0de77d1fe2d9eed0cac9ee3eaa7e6f026553d0
Darth-Kylokun <ftwkrc@gmail.com>
2022-07-24 04:38:09 -0400

    Client connects to server finally

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
+use actix::Actor;
 use actix_web::{middleware, App, HttpServer, web};
 use diesel::{r2d2::{ConnectionManager, self}, SqliteConnection};
-use ldl_server::app_config::config_app;
+use ldl_server::{app_config::config_app, server};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,11 +14,14 @@ async fn main() -> std::io::Result<()> {
         .build(manager)
         .expect("Failed to create pool.");
 
+    let server = server::ChatServer::new().start();
+
     log::info!("Starting HTTP server at http://localhost:8080");
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(server.clone()))
             .configure(config_app)
             .wrap(middleware::Logger::default())
     })
